d7e891cd543ffecfd3b03160020e102470ce318b
circir9 <c1123chuang@gmail.com>
2024-04-11 16:51:12 +0800

    fix

--- server/Controllers/VideoController.cs
+++ server/Controllers/VideoController.cs
@@ -17,11 +17,15 @@ public class VideoController : ControllerBase{
 
     [HttpGet]
     public async Task<IActionResult> Get(){
-        var filename = "test.mp4";  
-        //Build the File Path.  
-        string filepath = Path.Combine(Directory.GetCurrentDirectory(), _SaveFilesDir, filename);
+        var filename = "test.mp4";
+        string filepath = Path.Combine(Directory.GetCurrentDirectory(), _SaveFilesDir);
+        if(!Directory.Exists(filepath)){
+            Directory.CreateDirectory(filepath);
+        }
+        //Build the File Path.
+        var exactpath = Path.Combine(Directory.GetCurrentDirectory(), _SaveFilesDir, filename);
     
-        var filestream = System.IO.File.OpenRead(filepath);
+        var filestream = System.IO.File.OpenRead(exactpath);
         return File(filestream, "video/mp4", fileDownloadName: filename, enableRangeProcessing: true);
     }
 }
